8d74eec7f51497d5904ff3eb988de5629c93bb26
yashu55 <ysp551996@gmail.com>
2020-09-03 01:56:26 +0530

    form 1 done

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-signup',
@@ -6,8 +9,51 @@ import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-  constructor() {}
+  public faUserPlus = faUserPlus;
+  public uiInvalidCredential = false;
+
+  public fbFormGroup = this.fb.group({
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern('^[a-zAZ0-9]+@[a-zAZ0-9]+$'),
+      ],
+    ],
+    password: ['', Validators.required],
+    mobile: ['', Validators.required],
+  });
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {}
-  public faUserPlus = faUserPlus;
+
+  async registerHere() {
+    const data = this.fbFormGroup.value;
+    console.log(data);
+    const url = 'http://localhost:3000/adduser';
+    try {
+      const result: any = await this.http.post(url, data).toPromise();
+      if (result.message) {
+        this.uiInvalidCredential = true;
+        this.fbFormGroup.reset();
+      } else {
+        this.fbFormGroup.reset();
+
+        throw new Error();
+      }
+    } catch (err) {
+      this.fbFormGroup.reset();
+      console.log('Unable to connect to server');
+    }
+
+    return false;
+  }
 }
