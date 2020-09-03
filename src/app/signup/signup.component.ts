a837f06a87eb6fa3577b66d352894729b5158a81
yashu55 <ysp551996@gmail.com>
2020-09-03 23:44:41 +0530

    signup done

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
@@ -11,22 +11,96 @@ import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
 export class SignupComponent implements OnInit {
   public faUserPlus = faUserPlus;
   public uiInvalidCredential = false;
+  public responseVar = '';
+  public errorDiv = true;
+  MustMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
 
-  public fbFormGroupSignup = this.form.group({
-    firstname: ['', Validators.required],
-    lastname: ['', Validators.required],
-    email: [
-      '',
-      [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.pattern('^[a-zAZ0-9]+@[a-zAZ0-9]+$'),
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        // return if another validator has already found an error on the matchingControl
+        return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
+  }
+
+  public fbFormGroupSignup: FormGroup = this.form.group(
+    {
+      firstname: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(10),
+          Validators.pattern('^[a-zA-Z]+$'),
+        ],
       ],
-    ],
-    password: ['', Validators.required],
-    mobile: ['', Validators.required],
-  });
-
+      lastname: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(10),
+          Validators.pattern('^[a-zA-Z]+$'),
+        ],
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(30),
+          Validators.pattern(
+            '^[a-zA-Z0-9_\\.]+@[a-zA-Z0-9_\\.]+(\\.[a-zA-Z0-9_\\.]+)+$'
+          ),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20),
+          Validators.pattern(
+            '[\\S]*?[A-Z]+?[\\S]*?[a-z]*?[\\S]*?[0-9]*?[\\S]*?[^a-zA-Z0-9]*?[\\S]*?'
+          ),
+          //  Validators.pattern('^(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$'),
+        ],
+      ],
+      cpassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(20),
+          Validators.pattern(
+            '[\\S]*?[A-Z]+?[\\S]*?[a-z]*?[\\S]*?[0-9]*?[\\S]*?[^a-zA-Z0-9]*?[\\S]*?'
+          ),
+          //   Validators.pattern('^(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$'),
+        ],
+      ],
+      mobile: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(10),
+          Validators.maxLength(10),
+          Validators.pattern('[0-9]{10}'),
+          //   Validators.pattern('^(?=.*d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$'),
+        ],
+      ],
+    },
+    {
+      validator: this.MustMatch('password', 'cpassword'),
+    }
+  );
   constructor(
     private form: FormBuilder,
     private router: Router,
@@ -42,16 +116,22 @@ export class SignupComponent implements OnInit {
     try {
       const result: any = await this.http.post(url, data).toPromise();
       if (result.message) {
+        alert('Welcome:-)\nYou have registered successfully!!');
+        this.fbFormGroupSignup.reset();
+        this.router.navigate(['login']);
+      } else if (result.message == 'registered') {
+        this.fbFormGroupSignup.reset();
+        this.responseVar = 'Email address is already registered!!';
         this.uiInvalidCredential = true;
-        this.fbFormGroupSignup.reset();
-      } else {
-        this.fbFormGroupSignup.reset();
-
-        throw new Error();
+        this.errorDiv = false;
       }
     } catch (err) {
       this.fbFormGroupSignup.reset();
       console.log('Unable to connect to server');
+      this.responseVar =
+        ':-( Unable to connect to server. Please try again later.';
+      this.uiInvalidCredential = true;
+      this.errorDiv = false;
     }
 
     return false;
